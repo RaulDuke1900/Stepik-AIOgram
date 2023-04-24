710a8dfaacff15108546670240ce8c7a10cebc4a
Artem Gezha <gezha@ukr.net>
2023-04-24 15:21:22 +0300

    Change config.py

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ class TgBot:
 class Config:
     tg_bot: TgBot
 
+# Создаем функцию, которая будет читать файл .env и возвращать экземпляр класса Config с заполненными полями token и admin_ids
 def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
